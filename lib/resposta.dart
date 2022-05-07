d22f9084e9b7131e3cd05768f28ef1c8d921ccdf
Gabriel Santos <gabrielsantos98898@gmail.com>
2022-05-07 19:50:55 -0300

    Adicionando o parametro de funcao para colocar no onPressed

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -5,16 +5,23 @@ import 'package:flutter/material.dart';
 class Resposta extends StatelessWidget {
 
   final String texto;
+  final void Function() onSelecao;
 
-  Resposta(this.texto);
+  Resposta(this.texto, this.onSelecao);
 
   
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-              child: Text(texto),
-              onPressed: null,
-            );
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: RaisedButton(
+        textColor: Colors.white,
+        color: Colors.blue,
+        child: Text(texto),
+        onPressed: onSelecao
+      ),
+    );
   }
 }
